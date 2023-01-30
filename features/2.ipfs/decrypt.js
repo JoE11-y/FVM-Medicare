092449298f28930328d03b596e2bcfe96389f15d
JoE11-y <josephedoh77@gmail.com>
2023-01-30 07:08:35 +0100

    add encryption n decryption

---
+++ features/2.ipfs/decrypt.js
@@ -0,0 +1,54 @@
+require("dotenv").config({ path: "../.env" });
+// Decrypt file nodejs
+const { ethers } = require("ethers");
+const fs = require("fs");
+const lighthouse = require("@lighthouse-web3/sdk");
+const js_base64 = require("js-base64");
+
+const b64_to_json = (b64) => {
+  var u8arr = js_base64.Base64.toUint8Array(b64);
+
+  var jsonBuf = Buffer.from(u8arr);
+
+  var jsonFile = JSON.parse(jsonBuf.toString());
+
+  return jsonFile;
+};
+
+const sign_auth_message = async (publicKey, privateKey) => {
+  const provider = new ethers.providers.JsonRpcProvider();
+  const signer = new ethers.Wallet(privateKey, provider);
+  const messageRequested = (await lighthouse.getAuthMessage(publicKey)).data
+    .message;
+  const signedMessage = await signer.signMessage(messageRequested);
+  return signedMessage;
+};
+
+const decrypt = async () => {
+  const cid = "QmR2EViJjaNJtzEkDMddVhreWVA3cFk58JkFBCwVHq41oM";
+  const publicKey = "0xf3a7050c41c7C8e06Ec82FCAffBE62bfE8D84D96";
+  const privateKey = process.env.PRIVATE_KEY;
+
+  // Get file encryption key
+  const signed_message = await sign_auth_message(publicKey, privateKey);
+  const fileEncryptionKey = await lighthouse.fetchEncryptionKey(
+    cid,
+    publicKey,
+    signed_message
+  );
+
+  // Decrypt File
+  const decrypted = await lighthouse.decryptFile(
+    cid,
+    fileEncryptionKey.data.key
+  );
+
+  // Save File
+  var b64 = Buffer.from(decrypted);
+
+  const jsonFile = b64_to_json(b64.toString());
+
+  console.log(jsonFile);
+};
+
+decrypt();
--- features/2.ipfs/encrypt.js
+++ features/2.ipfs/encrypt.js
@@ -1,62 +1,51 @@
-// //Dependencies
-// //tweetnacl
-// //import nacl from "tweetnacl";
-// // import { packToBlob } from "ipfs-car/pack/blob";
-// // import { MemoryBlockStore } from "ipfs-car/blockstore/memory";
+require("dotenv").config({ path: "../.env" });
+const { ethers } = require("ethers");
+const lighthouse = require("@lighthouse-web3/sdk");
+const js_base64 = require("js-base64");
 
-// //types- record: U8Arr, name: string, encryption: U8Arr
-// export const encryptRecordAndPin = async (
-//   record,
-//   recordIdentifier,
-//   signature
-// ) => {
-//   const encrypted = nacl.secretbox(
-//     record,
-//     new Uint8Array(24),
-//     signature.slice(0, 32)
-//   );
+var obj = {
+  employees: [
+    { firstName: "John", lastName: "Doe" },
+    { firstName: "Anna", lastName: "Smith" },
+    { firstName: "Peter", lastName: "Jones" },
+  ],
+};
 
-//   return pinFile({ file: encrypted, recordIdentifier });
-// };
+const get_b64 = (jsonObj) => {
+  var buf = Buffer.from(JSON.stringify(jsonObj));
 
-// // returns the cid of the pinned file
-// export const pinFile = async (file, name) => {
-//   const { root, car } = await packToBlob({
-//     input: file,
-//     blockstore: new MemoryBlockStore(),
-//     wrapWithDirectory: false,
-//   });
+  var arr = new Uint8Array(buf);
 
-//   const fd = new FormData();
-//   fd.append("file", car, name);
+  var b64 = js_base64.Base64.fromUint8Array(arr);
 
-//   const res = await fetch("/api/upload", {
-//     method: "POST",
-//     body: fd,
-//   });
+  return b64;
+};
 
-//   if (!res.ok) {
-//     throw new Error("Unable to pin file to IPFS");
-//   }
+const sign_auth_message = async (publicKey, privateKey) => {
+  const provider = new ethers.providers.JsonRpcProvider();
+  const signer = new ethers.Wallet(privateKey, provider);
+  const messageRequested = (await lighthouse.getAuthMessage(publicKey)).data
+    .message;
+  const signedMessage = await signer.signMessage(messageRequested);
+  return signedMessage;
+};
 
-//   return root.toV1().toString();
-// };
+const deployEncrypted = async () => {
+  const b64Text = get_b64(obj);
+  const apiKey = process.env.LIGHTHOUSE_KEY;
+  const publicKey = "0xf3a7050c41c7C8e06Ec82FCAffBE62bfE8D84D96";
+  const privateKey = process.env.PRIVATE_KEY;
+  const signed_message = await sign_auth_message(publicKey, privateKey);
 
-// // returns the user record data
-// // params: cid - string, encryptionBytes - U8Arr
-// export const downloadAndDecrypt = async (cid, signature) => {
-//   const res = await fetch(`https://w3s.link/ipfs/${cid}`);
-//   if (!res.ok) {
-//     throw new Error("Could not fetch record from IPFS");
-//   }
+  const response = await lighthouse.textUploadEncrypted(
+    b64Text,
+    apiKey,
+    publicKey,
+    signed_message
+  );
 
-//   const record = nacl.secretbox.open(
-//     new Uint8Array(await res.arrayBuffer()),
-//     new Uint8Array(24),
-//     signature.slice(0, 32)
-//   );
+  // Display response
+  console.log(response);
+};
 
-//   if (!record) throw new Error("record decryption failed");
-
-//   return record;
-// };
+deployEncrypted();
