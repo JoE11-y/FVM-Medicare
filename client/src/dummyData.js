8ee74ed8329c38a9ec2385eaae2261a6d92278a4
Osaretin Frank <osaretin.frank10@gmail.com>
2023-02-02 00:13:23 +0100

    added records dummy data

--- client/src/dummyData.js
+++ client/src/dummyData.js
@@ -33,3 +33,44 @@ export const patients = [
     image: patient4,
   },
 ]
+
+export const doctors = []
+
+export const records = [
+  {
+    type: "Patient Information Leaflet",
+    preview: ["Pharmacy", "Medicine Side Effects", "dosage"],
+    updated: "27.01.2023",
+    created: "23.01.2020",
+  },
+  {
+    type: "Previous Discharge Summary",
+    preview: ["Test Result", "Reason for admission", "medical advice"],
+    updated: "27.01.2023",
+    created: "23.01.2020",
+  },
+  {
+    type: "Recent Medical Test",
+    preview: ["Blood Test", "Urine Test", "Heart Test"],
+    updated: "27.01.2023",
+    created: "23.01.2020",
+  },
+  {
+    type: "Mental Status Examination",
+    preview: ["Mental Health", "Hospital", "Rehab summary"],
+    updated: "27.01.2023",
+    created: "23.01.2020",
+  },
+  {
+    type: "Operative Report",
+    preview: ["Surgical Operation", "Procedures", "Complications"],
+    updated: "27.01.2023",
+    created: "23.01.2020",
+  },
+  {
+    type: "Medical Record History",
+    preview: ["Notes and Remarks", "Prescription", "Doctor"],
+    updated: "27.01.2023",
+    created: "23.01.2020",
+  },
+]
