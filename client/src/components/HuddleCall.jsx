9fb8cdeb538b3de6b643abcc9875944b0d0636bc
JoE11-y <josephedoh77@gmail.com>
2023-02-02 12:20:02 +0100

    add huddle iframe

---
+++ client/src/components/HuddleCall.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+import { huddleIframeApp, HuddleIframe } from "@huddle01/huddle01-iframe";
+
+export const HuddleCall = (meetId) => {
+  const iframeConfig = {
+    roomUrl: `https://iframe.huddle01.com/${meetId}`,
+    height: "600px",
+    width: "80%",
+  };
+
+  useEffect(() => {
+    huddleIframeApp.on("peer-join", (data) =>
+      console.log({ iframeData: data })
+    );
+    huddleIframeApp.on("peer-left", (data) =>
+      console.log({ iframeData: data })
+    );
+  }, []);
+
+  return (
+    <div className="container">
+      <HuddleIframe config={iframeConfig} />
+    </div>
+  );
+};
